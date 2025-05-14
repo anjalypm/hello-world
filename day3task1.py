ff306e39ced446080cd98880c00aa475507028dd
anjalypm <anjalypm3961@gmail.com>
2025-05-14 12:54:12 +0530

    Add files via upload

---
+++ day3task1.py
@@ -0,0 +1,10 @@
+print("day3task1")
+num=int(input("ENTER THE NUMBER:"))
+if num==0:
+    print("Number is Zero")
+elif num>0:
+    print("Number is positve")
+elif num<0:
+    print("Number is negative")
+else:
+    print("its an invalid number")
---
+++ day3task5.py
@@ -0,0 +1,5 @@
+num=int(input("ENTER THE NUMBER:"))
+if num%3==0 and num%5==0:
+    print(f"{num} is divisible by 3 & 5")
+else :
+    print(f"{num} is not divisible by 3 & 5")
